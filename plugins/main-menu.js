ec82cb607a653521c100925813e14b4405ee53a8
RyanBotzz <109885043+RyanBotzz@users.noreply.github.com>
2022-08-03 18:41:11 +0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -285,7 +285,7 @@ const fdoc = {
 if (teks == '404') {
 let menuu = `┌────「 *${namebot}* 」───⬣
 │⬡ Aktif selama ${uptime}
-│⬡ ${Object.keys(global.db.data.users).length} Pengguna
+│⬡ ${Object.keys(global.db.data.users).length} Total Pengguna
 │⬡ Mode : ${global.opts['self'] ? 'Self' : 'publik'}
 │⬡ ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} Chat Terbanned
 │⬡ ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} Pengguna Terbanned
