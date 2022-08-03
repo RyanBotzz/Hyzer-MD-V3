aa9a40b8650fb616a8d55b34cb6d56ccadbd16c1
RyanBotzz <109885043+RyanBotzz@users.noreply.github.com>
2022-08-03 08:34:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,12 +14,12 @@ global.gopay = '085779150501'
 
 //=============『 Info Bot 』=================//
 global.namebot = 'Hanz Bot'
-global.gc = 'bit.ly/HanzBot-Group'
+global.gc = 'https://chat.whatsapp.com/JCgXlfkzK6p6xIYk5ij0Pa'
 global.web = 'https://github.com/RyanBotzz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
+global.fotonya1 = 'https://telegra.ph/file/b0bbf4e101e058bdbe430.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
@@ -31,7 +31,7 @@ global.htki = '––––––『'
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wait = '_*di proses Tod...*_'
+global.wait = '_*di proses Tod🗿*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
